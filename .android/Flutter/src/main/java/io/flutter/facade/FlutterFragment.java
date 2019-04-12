a33961441f8f8f3c92a1871dbab206ae677249c8
xcz <xcz1203@163.com>
2019-04-12 19:13:21 +0800

    init

--- .android/Flutter/src/main/java/io/flutter/facade/FlutterFragment.java
+++ .android/Flutter/src/main/java/io/flutter/facade/FlutterFragment.java
@@ -2,12 +2,12 @@ package io.flutter.facade;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import io.flutter.view.FlutterView;
 
 /**
@@ -17,24 +17,24 @@ import io.flutter.view.FlutterView;
  * DO NOT EDIT.</p>
  */
 public class FlutterFragment extends Fragment {
-  public static final String ARG_ROUTE = "route";
-  private String mRoute = "/";
+    public static final String ARG_ROUTE = "route";
+    private String mRoute = "/";
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    if (getArguments() != null) {
-      mRoute = getArguments().getString(ARG_ROUTE);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mRoute = getArguments().getString(ARG_ROUTE);
+        }
     }
-  }
 
-  @Override
-  public void onInflate(Context context, AttributeSet attrs, Bundle savedInstanceState) {
-    super.onInflate(context, attrs, savedInstanceState);
-  }
+    @Override
+    public void onInflate(Context context, AttributeSet attrs, Bundle savedInstanceState) {
+        super.onInflate(context, attrs, savedInstanceState);
+    }
 
-  @Override
-  public FlutterView onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return Flutter.createView(getActivity(), getLifecycle(), mRoute);
-  }
+    @Override
+    public FlutterView onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return Flutter.createView(getActivity(), getLifecycle(), mRoute);
+    }
 }
